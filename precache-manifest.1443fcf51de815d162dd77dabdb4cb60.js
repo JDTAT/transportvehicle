6c2c3e1415f9e11e0c99ae088d427fcec77f2eb5
JDTAT <jdsingh781@gmail.com>
2020-08-14 08:58:42 +0530

    Updates

---
+++ precache-manifest.1443fcf51de815d162dd77dabdb4cb60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd1d633e66092041f9775ca5e53f754",
+    "url": "/transportvehicle/index.html"
+  },
+  {
+    "revision": "0763ef2f8d9951e714ae",
+    "url": "/transportvehicle/static/css/main.838c58d5.chunk.css"
+  },
+  {
+    "revision": "76e43f4f1d1eacde7553",
+    "url": "/transportvehicle/static/js/2.a1e28df5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/transportvehicle/static/js/2.a1e28df5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0763ef2f8d9951e714ae",
+    "url": "/transportvehicle/static/js/main.ae2c3c34.chunk.js"
+  },
+  {
+    "revision": "9c095c032b13af7061dc",
+    "url": "/transportvehicle/static/js/runtime-main.2531e121.js"
+  }
+]);
