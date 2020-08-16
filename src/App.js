bb1d9b648c68eea63ad5e33752566d15b73b663e
JDTAT <jdsingh781@gmail.com>
2020-08-16 18:11:57 +0530

    code updated with transport vehicle.

--- src/App.js
+++ src/App.js
@@ -1,186 +1,252 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Axios from 'axios';
+import { Card, Input, Form, Button, message, Tag, Row, Col, DatePicker } from 'antd'
+import "antd/dist/antd.css";
+import { CheckCircleOutlined } from '@ant-design/icons'
+import arrow from './arrow.png'
+
 
 const App = props => {
 
-  let [listData, setListData] = useState([]);
-  let [filterYear, setFilterYear] = useState();
-  let [launchStatus, setLaunchStatus] = useState();
-  let [landingStatus, setLandingStatus] = useState();
-  let [isLoading, setIsLoading] = useState(false);
-  // let url = "https://api.spaceXdata.com/v3/launches?limit=100&launch_success=true&land_success=true&launch_year=2014";
+  let [startDate, setStartDate] = useState();
+  let [tripSummary, setTripSummary] = useState();
+  let [totalTripTime, setTotalTripTime] = useState();
+
+  let url = "http://staging.watsoo.com:8080/watsoo-amazon-api//trip-controller-web/v1/vehicle/wise/summary/36";
+
+
+  function onStartDateChange(d, dateString) {
+    let date = new Date(dateString);
+    let time = date.getTime();
+    console.log("Start Date" + dateString);
+    console.log("Start Time" + time);
+  }
+
+  function onLastDateChange(d, dateString) {
+    let date = new Date(dateString);
+    let time = date.getTime();
+    console.log("Start Date" + dateString);
+    console.log("Start Time" + time);
+  }
+
 
 
   useEffect(() => {
-    let url;
-    let filerByYear;
-    let filerByLaunch;
-    let filerByLanding;
-
-    setIsLoading(!isLoading);
-
-    if (!!filterYear) {
-      filerByYear = "&launch_year=" + filterYear
-    } else {
-      filerByYear = ""
+    const body = {
+      "clientId": 10,
+      "dataRecord": {
+        "userRoleId": 4,
+        "userRoleName": "COMPANY",
+        "userId": 10
+      },
+      "fromDate": 1577888571659,
+      "toDate": 1593613371659,
+      "tripId": 36
     }
-
-    if (!!launchStatus) {
-      let realStatus = launchStatus.toLowerCase() === 'launchtrue' ? true : false
-      filerByLaunch = "&launch_success=" + realStatus
-    } else {
-      filerByLaunch = ""
-    }
-
-    if (!!landingStatus) {
-      let realStatus = landingStatus.toLowerCase() === 'true' ? true : false
-      filerByLanding = "&land_success=" + realStatus
-    } else {
-      filerByLanding = ""
-    }
-
-    url = "https://api.spacexdata.com/v3/launches?limit=100" + filerByLaunch + "" + filerByLanding + "" + filerByYear
-
-    Axios.get(url)
+    Axios.post(url, body)
       .then((response) => {
-        setIsLoading(false);
-        setListData(response.data);
+        // setIsLoading(false);
+        // setListData(response.data);
+        setTripSummary(response.data);
+        console.log(response.data);
       })
       .then((err) => {
-        setIsLoading(false);
+        // setIsLoading(false);
       })
       .catch(err => {
         console.log(err);
-        setIsLoading(false);
+        // setIsLoading(false);
       });
-  }, [filterYear, launchStatus, landingStatus]);
+  }, []);
 
   useEffect(() => {
 
-    let yearBox = document.getElementsByClassName("btn");
-    for (var a = 0; a < yearBox; a++) {
-      yearBox[a].style.backgroundColor = "yellow"
-    }
-  }, []);
+    // let totalTrip = (tripSummary && tripSummary.data) ? tripSummary.data.totalTripTime : null
+    // // let duration =trip.startTripDate;
+    // let totalTripMin = Math.floor((totalTrip / (1000 * 60)) % 60),
+    // totalTripHour = Math.floor((totalTrip / (1000 * 60 * 60)) % 24);
+    // totalTripHour = (totalTripHour > 0 ) ? totalTripHour + "hrs": null;
+    // totalTripMin = (totalTripMin > 0 ) ? totalTripMin + "min": "00 min";
+    // let finalTotalTripTime = totalTripHour+" "+totalTripMin;
+    // setTotalTripTime({finalTotalTripTime})
 
-  const onYearChange = (e) => {
-    let val = e.target.id;
-    console.log(e.target.id);
-    setFilterYear(val);
-
-    let yearBox = document.getElementsByClassName("btn");
-    for (var a = 0; a < yearBox.length; a++) {
-      yearBox[a].style.backgroundColor = "#adf5ad";
-      document.getElementById(val).style.backgroundColor = "#a2a20c"
-    }
-
-
-  }
-
-  const onLaunchChange = (e) => {
-    let val = e.target.id;
-    setLaunchStatus(val);
-    let yearBox = document.getElementsByClassName("launchbtn");
-    for (var a = 0; a < yearBox.length; a++) {
-      yearBox[a].style.backgroundColor = "#adf5ad";
-      document.getElementById(val).style.backgroundColor = "#a2a20c"
-    }
-  }
-
-  const onLandingChange = (e) => {
-    let val = e.target.id;
-    setLandingStatus(val);
-    let yearBox = document.getElementsByClassName("landbtn");
-    for (var a = 0; a < yearBox.length; a++) {
-      yearBox[a].style.backgroundColor = "#adf5ad";
-      document.getElementById(val).style.backgroundColor = "#a2a20c"
-    }
-  }
+  })
 
 
   return (
     <div>
-      <h1>SpaceX Launch Programs </h1>
-      <div className="container">
-        {/* Filter Start*/}
-        <div className="filter_container">
-          <b>Filters</b>
-          <p style={{ textAlign: "center", margin: "0px" }}>Launch Year</p>
-          <hr style={{ width: "60%" }} />
-          <div className="txt_center">
-            <button id="2006" className="btn" onClick={onYearChange}>2006</button>
-            <button id="2007" className="btn" onClick={onYearChange}>2007</button>
-            <button id="2008" className="btn" onClick={onYearChange}>2008</button>
-            <button id="2009" className="btn" onClick={onYearChange}>2009</button>
-            <button id="2010" className="btn" onClick={onYearChange}>2010</button>
-            <button id="2011" className="btn" onClick={onYearChange}>2011</button>
-            <button id="2012" className="btn" onClick={onYearChange}>2012</button>
-            <button id="2013" className="btn" onClick={onYearChange}>2013</button>
-            <button id="2014" className="btn" onClick={onYearChange}>2014</button>
-            <button id="2015" className="btn" onClick={onYearChange}>2015</button>
-            <button id="2016" className="btn" onClick={onYearChange}>2016</button>
-            <button id="2017" className="btn" onClick={onYearChange}>2017</button>
-            <button id="2018" className="btn" onClick={onYearChange}>2018</button>
-            <button id="2019" className="btn" onClick={onYearChange}>2019</button>
-            <button id="2020" className="btn" onClick={onYearChange}>2020</button>
-          </div>
-
-          <div>
-            <p style={{ textAlign: "center", margin: "0px" }}>Successfull Launch</p>
-            <hr style={{ width: "60%" }} />
-            <div className="txt_center">
-              <button id="launchtrue" className="launchbtn" onClick={onLaunchChange}>True</button>
-              <button id="launchfalse" className="launchbtn" onClick={onLaunchChange}>False</button>
-            </div>
-          </div>
-
-          <div>
-            <p style={{ textAlign: "center", margin: "0px" }}>Successfull Landing</p>
-            <hr style={{ width: "60%" }} />
-            <div className="txt_center">
-              <button id="true" className="landbtn" onClick={onLandingChange}>True</button>
-              <button id="false" className="landbtn" onClick={onLandingChange}>False</button>
-            </div>
-          </div>
-
+      {console.log("=========")}
+      {console.log(tripSummary)}
+      {/* Header */}
+      <div className="top_header">
+        {/* left side */}
+        <div>
+          <h3 style={{ margin: "0px" }}>Trip Summary</h3>
+          <span style={{ color: "blue" }}>dashboard </span> / <span> Trip Summary</span>
         </div>
-        {/* Filter End */}
-        {/* List Start */}
-        <div className="list_container">
-          {isLoading ? <div>Loading......</div> :
-            <React.Fragment>
-              {listData.length > 0 ?
-                listData.map((item, index) => {
-                  let missionId = "Not mentioned";
-                  if (item.mission_id.length > 0) {
-                    missionId = item.mission_id;
-                  }
-                  return <div style={{ height: "500px", padding: "20px", backgroundColor: "white", borderRadius: "5px" }}>
-                    <div style={{ height: "200px" }}><img src={item.links.mission_patch_small} height="100%" width="100%" /></div>
-                    <div className="fnn"><b>{item.mission_name}#{item.flight_number}</b></div>
-                    <div>
-                      <b>Mission Ids</b>
-                      <ul>
-                        <li>{missionId}</li>
-                      </ul>
-                    </div>
-                    <div><span><b>Launch Year: </b> {item.launch_year}</span></div>
-                    <div><span><b>Successfull Launch:</b></span> {item.launch_success ? <span>true</span> : <span>false</span>}</div>
-                    <div><span><b>Successfull Landing:</b></span> {item.rocket.first_stage.cores[0].land_success ? <span>true</span> : <span>false</span>}</div>
-                  </div>
+        {/* left side */}
+        {/* right side */}
+        <div>
+          <div className="date_from_to">
 
-
-                })
-                : <div>No data found</div>}
-            </React.Fragment>
-          }
-
+            <DatePicker onChange={onStartDateChange} className="mr-10" /> TO <DatePicker onChange={onLastDateChange} className="mr-10" />
+          </div>
         </div>
-        {/* List End */}
+        {/* right side */}
 
       </div>
-      <div className="txt_center"><b>Developed By</b>: Joginder Singh</div>
+      {/* Header */}
+
+      <div>
+        <div>
+          <div style={{ display: "flex", marginRight: "5px", marginBottom: "5px" }}>
+            <Tag icon={<CheckCircleOutlined />} color="success" className="box_top box_one">
+              {tripSummary && tripSummary.data ? <span>{tripSummary.data.vehicleNo}</span> : null}</Tag>
+            <Tag icon={<CheckCircleOutlined />} color="success" className="box_top box_two">
+              Total Trips :
+            {tripSummary && tripSummary.data ? <span> {tripSummary.data.totalTrips}</span> : null}
+            </Tag>
+            <Tag icon={<CheckCircleOutlined />} color="success" className="box_top box_three">
+              {tripSummary && tripSummary.data ? <span> {tripSummary.data.totalKm} KM</span> : null}
+            </Tag>
+          </div>
+          <div style={{ display: "flex", marginRight: "5px" }}>
+            <Tag icon={<CheckCircleOutlined />} color="success" className="box_top box_four">
+              Trip Time: {(tripSummary && tripSummary.data) ?
+                <span>
+                  {Math.floor((tripSummary.data.totalTripTime / (1000 * 60 * 60)) % 24) > 0 ?
+                    <span>{Math.floor((tripSummary.data.totalTripTime / (1000 * 60 * 60)) % 24)} hrs </span>
+                    : null}
+                  {Math.floor((tripSummary.data.totalTripTime / (1000 * 60)) % 60) > 0 ?
+                    <span> {Math.floor((tripSummary.data.totalTripTime / (1000 * 60)) % 60)} Min</span>
+                    : null}
+                </span> : null}
+            </Tag>
+            <Tag icon={<CheckCircleOutlined />} color="success" className="box_top box_five">
+              Trip Time: {(tripSummary && tripSummary.data) ?
+                <span>
+                  {Math.floor((tripSummary.data.totalTime / (1000 * 60 * 60)) % 24) > 0 ?
+                    <span>{Math.floor((tripSummary.data.totalTime / (1000 * 60 * 60)) % 24)} hrs </span>
+                    : null}
+                  {Math.floor((tripSummary.data.totalTime / (1000 * 60)) % 60) > 0 ?
+                    <span> {Math.floor((tripSummary.data.totalTime / (1000 * 60)) % 60)} Min</span>
+                    : null}
+                </span> : null}
+            </Tag>
+            <Tag icon={<CheckCircleOutlined />} color="success" className="box_top box_six">
+              Total EXP : {(tripSummary && tripSummary.data) ? <span>Rs. {tripSummary.data.totalExpences}</span> : null}
+            </Tag>
+          </div>
+        </div>
+      </div>
+
+
+      {/* Table Card */}
+      {(tripSummary && tripSummary.data && tripSummary.data.tripDetails) && tripSummary.data.tripDetails.length > 0 ?
+        tripSummary.data.tripDetails.map((item, index) => {
+          let tripStart = tripSummary.data.tripDetails[index].startDay;
+          let tripEnd = tripSummary.data.tripDetails[index].endDay;
+          let diffInTrip = tripEnd - tripStart
+
+
+          return <div style={{ marginTop: "10px", paddingBottom: "10px", backgroundColor: "white" }}>
+            <div style={{
+              height: "40px", backgroundColor: "#02023c",
+              color: "white", display: "flex", alignItems: "center", paddingLeft: "10px"
+            }}>
+              Date : &nbsp; {(tripSummary && tripSummary.data && tripSummary.data.tripDetails) && tripSummary.data.tripDetails.length > 0
+                ? <span>
+                  {(new Date(parseInt(tripSummary.data.tripDetails[index].startDay))).toLocaleString().split(",")[0]}&nbsp;
+               at {(new Date(parseInt(tripSummary.data.tripDetails[index].startDay))).toLocaleString().split(",")[1]} &nbsp;
+               {(new Date(parseInt(tripSummary.data.tripDetails[index].endDay))).toLocaleString().split(",")[0]}&nbsp;
+               at {(new Date(parseInt(tripSummary.data.tripDetails[index].endDay))).toLocaleString().split(",")[1]}
+                </span> : null
+              }&nbsp;
+              ({Math.floor((diffInTrip / (1000 * 60 * 60)) % 24) > 0 ?
+                <span>{Math.floor((diffInTrip / (1000 * 60 * 60)) % 24)} hrs &nbsp;</span>
+                : null}
+                    {Math.floor((diffInTrip / (1000 * 60)) % 60) > 0 ?
+                    <span>{Math.floor((diffInTrip / (1000 * 60)) % 60)} Minutes &nbsp;</span> 
+                    : <span>00 Minutes</span>})
+            </div>
+            {/* Table Card Header */}
+
+            {/* Table */}
+            <div style={{ margin: "10px" }}>
+              <table className="trip_table">
+                <tr>
+                  <th>#</th>
+                  <th>Trip Start node to End Node</th>
+                  <th>river Name</th>
+                  <th>Trip Expenses</th>
+                  <th>Trip Km</th>
+                  <th>Trip GPS Km</th>
+                  <th>Trip Time</th>
+                  <th>Odometer Reading</th>
+                  <th>Action</th>
+                </tr>
+                <tbody>
+                  {item.tripLists && item.tripLists.length > 0 ?
+                    item.tripLists.map((trip, tripindex) => {
+                      // let startDate = new Date(parseInt(trip.startTripDate))
+                      // console.log(startDate.getHours);
+                      // let startDateStr = (startDate.toLocaleString()).split(",");
+                      // let time =startDateStr.split("")
+                      //   let duration =trip.startTripDate;
+                      //   var milliseconds = parseInt((duration % 1000) / 100),
+                      //   seconds = Math.floor((duration / 1000) % 60),
+                      //   minutes = Math.floor((duration / (1000 * 60)) % 60),
+                      //   hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+                      // hours = (hours < 10) ? "0" + hours : hours;
+                      // minutes = (minutes < 10) ? "0" + minutes : minutes;
+                      // seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+
+                      // console.log("date is : " + hour + ":" + minute + ":" + seconds + "." );
+                      // console.log(trip.tripExpenses[tripindex])
+
+                      let tripTime = trip.tripRunningTime;
+                      // let duration =trip.startTripDate;
+                      var milliseconds = parseInt((tripTime % 1000) / 100),
+                        seconds = Math.floor((tripTime / 1000) % 60),
+                        minutes = Math.floor((tripTime / (1000 * 60)) % 60),
+                        hours = Math.floor((tripTime / (1000 * 60 * 60)) % 24);
+
+                      // hours = (hours < 10) ? "0" + hours : hours;
+                      // minutes = (minutes < 10) ? "0" + minutes : minutes;
+                      // seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+                      return <tr>
+                        <td>{tripindex + 1}</td>
+                    <td>{trip.startPointNode}  <img src={arrow} height="20px" width="20px" style={{ marginLeft: "10px", marginRight: "10px" }} />{trip.endPointNode}</td>
+                        <td>{trip.driverName}</td>
+                        <td>{trip.tripExpenses && trip.tripExpenses.length > 0 ? <span>Rs. {trip.tripExpenses[tripindex].amount}</span> : <span>Rs. 0</span>}</td>
+                        <td>{trip.totalKm} Km</td>
+                        <td>{trip.gpsDistance} Km</td>
+                        <td>{hours > 0 ? <span>{hours + " hrs "}</span> : null}
+                          {minutes > 0 ? <span>{minutes + " min"}</span> : null}</td>
+                        <td style={{ textAlign: "center" }}>{trip.startODOMeter} <img src={arrow} height="20px" width="20px" style={{ marginLeft: "10px", marginRight: "10px" }} /> {trip.endODOMeter}</td>
+                        <td style={{ textAlign: "center" }} >
+                          <div>
+                            <button style={{ marginRight: "5px" }} className="tbl_btn">Movement Report</button>
+                            <button className="tbl_btn">Stoppage Report</button>
+                          </div>
+                        </td>
+                      </tr>
+                    }) : null
+                  }
+
+                </tbody>
+              </table>
+            </div>
+            {/* Table */}
+          </div>
+        }) : <div>Error</div>
+      }
+
+      {/* Table Card */}
     </div>
   );
 }
